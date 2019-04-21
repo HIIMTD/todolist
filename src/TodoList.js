115f761522207b8f0e50d580ec251bf490eb7418
De Tao <td950614@gmail.com>
2019-04-21 00:20:44 -0400

    +TodoItem.js *TodoList.js
    edit code by multiple content

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import './style.css'
+import './style.css';
+import TodoItem from './TodoItem';
 class TodoList extends Component {
 
     constructor(props) {
@@ -28,12 +29,12 @@ class TodoList extends Component {
                     {
                         this.state.list.map((item, index) => {
                             return (
-                                <li key={index}
-                                    onClick={this.handleItemDelete.bind(this,index)}
-                                    dangerouslySetInnerHTML={{__html:item}}
-                                >
-                                   
-                                </li>)
+                                <div>
+                                <TodoItem content = {item} index={index}
+                                deleteItem = {this.handleItemDelete.bind(this)}
+                                />
+                                </div>
+                            )
                         })
                     }
                 </ul>
