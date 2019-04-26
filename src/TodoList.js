b9e03dcc677b1e354b8d8cf86e47607afe167dc2
De Tao <td950614@gmail.com>
2019-04-25 22:01:02 -0400

    *index.js *reducer.js *TodoList.js
    use redux to add input to list

--- src/TodoList.js
+++ src/TodoList.js
@@ -8,27 +8,54 @@ import store from './store/index';
 
 class TodoList extends Component {
 
-        constructor(props){
-            super(props);
-            this.state = store.getState();
-            console.log( this.state)
-        }
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        // console.log(this.state)
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this)
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
 
     render() {
         return (
             <div style={{ marginTop: 10, marginLeft: 10 }}>
                 <div>
-                    <Input value = {this.state.inputValue} place
-holder='todoinfo' style={{ width: 300, marginRight: 10 }} />                    <Button type="primary">Submit</Button>
+                    <Input value={this.state.inputValue}
+                        placeholder='todoinfo'
+                        style={{ width: 300, marginRight: 10 }}
+                        onChange={this.handleInputChange} />
+                    <Button type="primary" onClick={this.handleBtnClick}>Submit</Button>
                 </div>
                 <List
-                    style = {{marginTop:10,width:300}}
+                    style={{ marginTop: 10, width: 300 }}
                     bordered
                     dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                 />
             </div>
         )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange() {
+        this.setState(store.getState())
+    }
+
+    handleBtnClick(){
+        const action = {
+            type: "add_todo_item"
+
+        };
+        store.dispatch(action);
     }
 }
 
